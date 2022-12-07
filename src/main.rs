591b7a93d23c7f857e95bac4b539f1bad80e4677
marihachi <marihachi0620@gmail.com>
2022-12-07 22:22:22 +0900

    add IR experiment of basic func

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ mod frontend;
 mod mir;
 mod runtime;
 
+// mod experiments {
+//     pub mod basic_fn;
+// }
+
 fn main() {
+    // experiments::basic_fn::run();
+
     let mut engine = Engine::new();
     engine.compile();
 }
