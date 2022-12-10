6f8b379d9dd4b0178d5e3a3a4a3c00771ffc77c7
marihachi <marihachi0620@gmail.com>
2022-12-10 19:55:33 +0900

    node

--- src/parser/node.rs
+++ src/parser/node.rs
@@ -1,58 +1,73 @@
+use super::Token;
+
+#[derive(Debug)]
 pub enum Node {
   Number(NumberNode),
   Call(CallNode),
   Func(FuncNode),
   VarDeclaration(VarDeclarationNode),
   FuncDeclaration(FuncDeclarationNode),
+  BinaryExpr(BinaryExprNode),
 }
 
 // Number
 
+#[derive(Debug)]
 pub struct NumberNode {
   value_int: u32,
 }
+
 pub fn number_node(value: u32) -> Node {
   Node::Number(NumberNode { value_int: value })
 }
 
 // Call
 
+#[derive(Debug)]
 pub struct CallNode {
   target_name: String,
   args: Vec<Node>,
 }
+
 pub fn call_node(target_name: String, args: Vec<Node>) -> Node {
   Node::Call(CallNode { target_name, args })
 }
 
 // Func
 
+#[derive(Debug)]
 pub struct FuncNode {
   // TODO: param types
   // TODO: return type
   children: Vec<Node>,
 }
+
 pub fn func_node(children: Vec<Node>) -> Node {
   Node::Func(FuncNode { children })
 }
 
 // VarDeclaration
 
+#[derive(Debug)]
 pub struct VarDeclarationNode {
   name: String,
   attributes: Vec<VarDeclarationAttr>,
   definition: Option<Box<Node>>,
 }
+
+#[derive(Debug)]
 pub enum VarDeclarationAttr {
   Const,
   Let,
 }
+
 pub fn var_declaration_node(name: String, attributes: Vec<VarDeclarationAttr>, definition: Option<Box<Node>>) -> Node {
   Node::VarDeclaration(VarDeclarationNode { name, attributes, definition })
 }
 
 // FuncDeclaration
 
+#[derive(Debug)]
 pub struct FuncDeclarationNode {
   name: String,
   // TODO: param types
@@ -60,10 +75,34 @@ pub struct FuncDeclarationNode {
   attributes: Vec<FuncDeclarationAttr>,
   definition: Option<Vec<Node>>,
 }
+
+#[derive(Debug)]
 pub enum FuncDeclarationAttr {
   Export,
   External,
 }
+
 pub fn declare_func_node(name: String, attributes: Vec<FuncDeclarationAttr>, definition: Option<Vec<Node>>) -> Node {
   Node::FuncDeclaration(FuncDeclarationNode { name, attributes, definition })
+}
+
+// Op
+
+#[derive(Debug)]
+pub struct BinaryExprNode {
+  op: OpKind,
+  left: Box<Node>,
+  right: Box<Node>,
+}
+
+#[derive(Debug)]
+pub enum OpKind {
+  Add,
+  Sub,
+  Mult,
+  Div,
+}
+
+pub fn binary_expr_node(op: OpKind, left: Box<Node>, right: Box<Node>) -> Node {
+  Node::BinaryExpr(BinaryExprNode { op, left, right })
 }
