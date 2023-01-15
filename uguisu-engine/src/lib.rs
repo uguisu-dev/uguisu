930e353fefbfb3575db0fcc47485fb53901ff16f
marihachi <marihachi0620@gmail.com>
2023-01-15 22:17:32 +0900

    implement interpreter

--- uguisu-engine/src/lib.rs
+++ uguisu-engine/src/lib.rs
@@ -18,18 +18,20 @@ impl CompileError {
 }
 
 pub fn run(code: &str) -> Result<(), String> {
-    println!("[Info] compiling ...");
-
+    println!("[Info] parsing ...");
     let ast = parse::parse(code).map_err(|e| format!("Compile Error: {}", e.message))?;
 
+    println!("[Info] code analyzing ...");
     let mut graph_source: HashMap<analyze::NodeId, analyze::Node> = HashMap::new();
     let mut analyzer = analyze::Analyzer::new(&mut graph_source);
     let graph = analyzer
         .translate(&ast)
         .map_err(|e| format!("Compile Error: {}", e.message))?;
 
+    println!("[Info] running ...");
+    let mut symbols = HashMap::new();
     let runner = run::Runner::new(&graph_source);
-    runner.run(&graph);
+    runner.run(&graph, &mut symbols);
 
     Ok(())
 }
--- uguisu-engine/src/run.rs
+++ uguisu-engine/src/run.rs
@@ -2,27 +2,31 @@ use crate::analyze::{LiteralValue, Node, NodeId};
 use crate::parse::Operator;
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Clone)]
 pub enum Value {
+    None,
     Number(i32),
     Function(NodeId),
 }
 
+enum StatementInfo {
+    None,
+    Return,
+    ReturnWith(Value),
+}
+
 pub struct Runner<'a> {
     graph_source: &'a HashMap<NodeId, Node>,
-    symbols: HashMap<NodeId, Value>,
 }
 
 impl<'a> Runner<'a> {
     pub fn new(graph_source: &'a HashMap<NodeId, Node>) -> Self {
         Self {
             graph_source,
-            symbols: HashMap::new(),
         }
     }
 
-    fn add_symbol(&mut self, node: NodeId, value: Value) {
-        self.symbols.insert(node, value);
+    fn set_symbol(symbols: &mut HashMap<NodeId, Value>, node: NodeId, value: Value) {
+        symbols.insert(node, value);
     }
 
     fn lookup_node(&self, node_id: NodeId) -> &Node {
@@ -30,42 +34,82 @@ impl<'a> Runner<'a> {
     }
 
     // 文を実行し、main関数の実行をする。
-    pub fn run(&self, graph: &Vec<NodeId>) {
-        // let mut func = None;
-        // for node in graph.iter() {
-        //     match self.lookup_node(node) {
-        //         Node::FunctionDeclaration(f) => {
-        //             if &f.identifier == "main" {
-        //                 func = Some(node);
-        //                 break;
-        //             }
-        //         }
-        //         _ => {}
-        //     }
-        // }
-        // match func {
-        //     Some(&x) => {
-        //         self.exec_statement(x);
-        //     }
-        //     None => {
-        //         println!("[Info] main function not found");
-        //     }
-        // }
-        todo!();
+    pub fn run(&self, graph: &Vec<NodeId>, symbols: &mut HashMap<NodeId, Value>) {
+        for &node_id in graph.iter() {
+            self.exec_statement(node_id, symbols);
+        }
+        // find main function
+        let mut func = None;
+        for &node in graph.iter() {
+            match self.lookup_node(node) {
+                Node::FunctionDeclaration(f) => {
+                    if &f.identifier == "main" {
+                        func = Some(node);
+                        break;
+                    }
+                }
+                _ => {}
+            }
+        }
+        match func {
+            Some(node_id) => {
+                self.call_func(node_id, symbols);
+            }
+            None => {
+                println!("[Info] main function not found");
+            }
+        }
+    }
+
+    fn call_func(&self, node_id: NodeId, symbols: &mut HashMap<NodeId, Value>) {
+        match self.lookup_node(node_id) {
+            Node::FunctionDeclaration(func) => {
+                match &func.body {
+                    Some(body) => {
+                        for &node_id in body.iter() {
+                            self.exec_statement(node_id, symbols);
+                        }
+                    }
+                    None => panic!("function body not found (node_id={})", node_id),
+                }
+            }
+            _ => panic!("function expected (node_id={})", node_id),
+        }
     }
 
     // 宣言の解釈やvariableへの登録をする。
-    pub fn exec_statement(&self, node_id: NodeId) {
-        // match self.lookup_node(node_id) {
-        //     Node::FunctionDeclaration(_) => {
-        //         Value::Function(node_id);
-        //     }
-        //     _ => panic!("unexpected statement node"),
-        // }
-        todo!();
+    fn exec_statement(&self, node_id: NodeId, symbols: &mut HashMap<NodeId, Value>) -> StatementInfo {
+        match self.lookup_node(node_id) {
+            Node::FunctionDeclaration(_) => {
+                Self::set_symbol(symbols, node_id, Value::Function(node_id));
+                StatementInfo::None
+            }
+            Node::VariableDeclaration(variable) => {
+                let a = self.eval_expr(variable.body, symbols);
+                Self::set_symbol(symbols, node_id, a);
+                StatementInfo::None
+            }
+            Node::ReturnStatement(Some(expr)) => {
+                let value = self.eval_expr(*expr, symbols);
+                StatementInfo::ReturnWith(value)
+            }
+            Node::ReturnStatement(None) => {
+                StatementInfo::Return
+            }
+            Node::Assignment(statement) => {
+                let value = self.eval_expr(statement.body, symbols);
+                Self::set_symbol(symbols, statement.dest, value);
+                StatementInfo::None
+            }
+            Node::Literal(_) | Node::BinaryExpr(_) | Node::CallExpr(_) | Node::FuncParamDeclaration(_) => {
+                self.eval_expr(node_id, symbols);
+                StatementInfo::None
+            }
+            _ => panic!("unexpected statement node (node_id={})", node_id),
+        }
     }
 
-    fn eval_expr(&self, node_id: NodeId) -> Value {
+    fn eval_expr(&self, node_id: NodeId, symbols: &mut HashMap<NodeId, Value>) -> Value {
         match self.lookup_node(node_id) {
             Node::Literal(literal) => {
                 match literal.value {
@@ -75,13 +119,13 @@ impl<'a> Runner<'a> {
                 }
             }
             Node::BinaryExpr(binary_expr) => {
-                let left = match self.eval_expr(binary_expr.left) {
+                let left = match self.eval_expr(binary_expr.left, symbols) {
                     Value::Number(n) => n,
-                    _ => panic!("number expected"),
+                    _ => panic!("number expected (node_id={})", node_id),
                 };
-                let right = match self.eval_expr(binary_expr.right) {
+                let right = match self.eval_expr(binary_expr.right, symbols) {
                     Value::Number(n) => n,
-                    _ => panic!("number expected"),
+                    _ => panic!("number expected (node_id={})", node_id),
                 };
                 match binary_expr.operator {
                     Operator::Add => Value::Number(left + right),
@@ -93,14 +137,15 @@ impl<'a> Runner<'a> {
             Node::CallExpr(call_expr) => {
                 let callee = match self.lookup_node(call_expr.callee) {
                     Node::FunctionDeclaration(func) => func,
-                    _ => panic!("function expected"),
+                    _ => panic!("function expected (node_id={})", node_id),
                 };
+                self.call_func(call_expr.callee, symbols);
                 todo!();
             }
-            Node::FuncParamDeclaration(func_param) => {
+            Node::FuncParamDeclaration(_) => {
                 todo!();
             }
-            _ => panic!("unexpected expr node"),
+            _ => panic!("unexpected expr node (node_id={})", node_id),
         }
     }
 }
