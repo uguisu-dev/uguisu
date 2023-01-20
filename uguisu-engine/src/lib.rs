7ed98f9aac67546548d9d26c94538ffc8a90ac42
marihachi <marihachi0620@gmail.com>
2023-01-21 03:56:54 +0900

    test

--- uguisu-engine/src/lib.rs
+++ uguisu-engine/src/lib.rs
@@ -69,21 +69,32 @@ mod test {
     }
 
     #[test]
-    fn test_empty_return() {
+    fn test_empty_function() {
         run_test(
             "
+            fn main() { }
+            ",
+        );
+    }
+
+    #[test]
+    fn test_function_basic() {
+        run_test(
+            "
+            fn add(x: number, y: number): number {
+                return x + y;
+            }
             fn main() {
-                return;
+                add(1, 2);
             }
             ",
         );
     }
 
     #[test]
-    fn text_basic() {
+    fn test_calc_with_function_1() {
         run_test(
             "
-            external fn print_num(value: number);
             fn add(x: number, y: number): number {
                 return x + y;
             }
@@ -91,35 +102,51 @@ mod test {
                 return x * x;
             }
             fn main() {
-                print_num(square(add(1, 2) * 3));
+                add(square(2), 3);
             }
             ",
         );
     }
 
     #[test]
-    fn text_variable() {
+    fn test_calc_with_function_2() {
         run_test(
             "
-            external fn print_num(value: number);
             fn square(x: number): number {
                 return x * x;
             }
+            fn calc(x: number, y: number): number {
+                return square(x) + y;
+            }
             fn main() {
-                const var1 = 2;
-                print_num(square(var1 * 3));
+                calc(2, 3);
             }
             ",
         );
+    }
+
+    #[test]
+    fn test_variable_basic() {
         run_test(
             "
-            external fn print_num(value: number);
-            fn square(x: number): number {
-                return x * x;
+            fn main() {
+                const x = 1 + 2;
+            }
+            ",
+        );
+    }
+
+    #[test]
+    fn test_calc_with_variable() {
+        run_test(
+            "
+            fn calc(x: number, y: number): number {
+                const temp = x + y;
+                return temp * temp;
             }
             fn main() {
-                const var1 = square(2 * 3);
-                print_num(var1);
+                const a = 2;
+                calc(a, 3);
             }
             ",
         );
