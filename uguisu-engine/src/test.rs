c76138256e7d723dc591edd2f082aa1dc5fefe2d
marihachi <marihachi0620@gmail.com>
2023-01-25 18:59:44 +0900

    test

--- uguisu-engine/src/test.rs
+++ uguisu-engine/src/test.rs
@@ -6,13 +6,13 @@ fn try_run_test(code: &str) -> Result<(), String> {
     // println!("[Info] parsing ...");
     let ast = match engine.parse(code) {
         Ok(x) => x,
-        Err(e) => return Err(format!("ParseError: {}", e.message)),
+        Err(e) => return Err(format!("Parser Error: {}", e.message)),
     };
 
     // println!("[Info] code analyzing ...");
     let graph = match engine.analyze(ast) {
         Ok(x) => x,
-        Err(e) => return Err(format!("AnalyzeError: {}", e.message)),
+        Err(e) => return Err(format!("Analyzer Error: {}", e.message)),
     };
 
     // println!("[Info] show graph map");
@@ -21,7 +21,7 @@ fn try_run_test(code: &str) -> Result<(), String> {
     // println!("[Info] running ...");
     match engine.run(graph) {
         Ok(x) => Ok(x),
-        Err(e) => return Err(format!("RunnerError: {}", e.message)),
+        Err(e) => return Err(format!("Runner Error: {}", e.message)),
     }
 }
 
