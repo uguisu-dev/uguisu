ac9431dc5ff8c4bce2e3b72bcfe40f6db65b65b2
marihachi <marihachi0620@gmail.com>
2023-02-20 11:27:02 +0900

    parser

--- src/ast.ts
+++ src/ast.ts
@@ -1,6 +1,7 @@
 export enum AstNodeKind {
 	Identifier,
 	Number,
+	IfStatement,
 }
 
 export interface AstNode {
@@ -13,15 +14,25 @@ export interface IdentifierNode extends AstNode {
 	name: string;
 }
 
+export function makeIdentifier(name: string, pos: number): IdentifierNode {
+	return { kind: AstNodeKind.Identifier, name, pos };
+}
+
 export interface NumberNode extends AstNode {
 	kind: AstNodeKind.Number;
 	value: string;
 }
 
-export function makeIdentifier(name: string, pos: number): IdentifierNode {
-	return { kind: AstNodeKind.Identifier, name, pos };
-}
-
 export function makeNumber(value: string, pos: number): NumberNode {
 	return { kind: AstNodeKind.Number, value, pos };
+}
+
+export interface IfStatementNode extends AstNode {
+	kind: AstNodeKind.IfStatement;
+	cond: AstNode;
+	thenBlock: AstNode[];
+}
+
+export function makeIfStatement(cond: AstNode, thenBlock: AstNode[], pos: number): IfStatementNode {
+	return { kind: AstNodeKind.IfStatement, cond, thenBlock, pos };
 }
--- src/parse.ts
+++ src/parse.ts
@@ -1,56 +1,170 @@
-import { AstNode, makeIdentifier, makeNumber } from './ast';
+import { AstNode, makeIdentifier, makeIfStatement, makeNumber } from './ast';
 import { Token, TokenKind } from './tokenize';
 
-function expectNumber(index: number, input: Token[]): string {
-	const token = input[index];
-	if (token.kind != TokenKind.Digits) {
-		throw new Error(`Digits is expected (index: ${token.pos})`);
-	}
-	return token.value;
+function isEof(index: number, input: Token[]) {
+	return index >= input.length;
 }
 
-function expectIdent(index: number, input: Token[]): string {
-	const token = input[index];
-	if (token.kind != TokenKind.Identifier) {
-		throw new Error(`Identifier is expected (index: ${token.pos})`);
+function getToken(index: number, input: Token[]): Token | null {
+	return input[index];
+}
+
+function nextToken(index: number, input: Token[]): [Token, number] | null {
+	if (isEof(index, input)) {
+		return null;
+	} else {
+		return [input[index], index + 1];
 	}
-	return token.value;
 }
 
 export function parse(offset: number, input: Token[]): AstNode[] {
 	let result;
+	let token;
 	let index = offset;
+
 	let accum: AstNode[] = [];
-	while (index < input.length) {
-		const token = input[index];
+	while (!isEof(index, input)) {
+		if ((token = getToken(index, input)) != null) {
 
-		result = parseExpr(index, input);
-		if (result != null) {
-			accum.push(result[0]);
-			index = result[1];
-			continue;
+			result = parseStatement(index, input);
+			if (result != null) {
+				accum.push(result[0]);
+				index = result[1];
+				continue;
+			}
+
+			throw new Error(`syntax error (index: ${token.pos})`);
 		}
-
-		throw new Error(`syntax error (index: ${token.pos})`);
 	}
 
 	return accum;
 }
 
-export function parseExpr(offset: number, input: Token[]): [AstNode, number] | null {
+function parseStatement(offset: number, input: Token[]): [AstNode, number] | null {
+	let result;
 	let index = offset;
-	const token = input[index];
 
-	if (token.kind == TokenKind.Digits) {
-		const node = makeNumber(token.value, token.pos);
-		index++;
+	if ((result = getToken(index, input)) != null) {
+		const token = result;
+		if (token.kind == TokenKind.Keyword && token.value == 'if') {
+			if ((result = parseIfStatement(index, input)) != null) {
+				return result;
+			}
+		}
+	}
+
+	return null;
+}
+
+export function parseExpr(offset: number, input: Token[]): [AstNode, number] | null {
+	let token;
+	let index = offset;
+
+	if ((token = getToken(index, input)) != null) {
+		if (token.kind == TokenKind.Digits) {
+			const node = makeNumber(token.value, token.pos);
+			index++;
+			return [node, index];
+		}
+
+		if (token.kind == TokenKind.Identifier) {
+			const node = makeIdentifier(token.value, token.pos);
+			index++;
+			return [node, index];
+		}
+	}
+
+	return null;
+}
+
+function parseIfStatement(offset: number, input: Token[]): [AstNode, number] | null {
+	let result;
+	let token;
+	let index = offset;
+
+	if ((result = parseIfCondBlock(index, input)) != null) {
+		const cond = result[0];
+		const thenBlock = result[1];
+		index = result[2];
+
+		// else if blocks
+
+		// else
+
+		const node = makeIfStatement(cond, thenBlock, offset);
 		return [node, index];
 	}
 
-	if (token.kind == TokenKind.Identifier) {
-		const node = makeIdentifier(token.value, token.pos);
-		index++;
-		return [node, index];
+	return null;
+}
+
+/**
+ * ```text
+ * "if" <cond> "{" <statement>... "}"
+ * ```
+*/
+function parseIfCondBlock(offset: number, input: Token[]): [AstNode, AstNode[], number] | null {
+	let result;
+	let token;
+	let index = offset;
+
+	// "if"
+	if ((token = getToken(index, input)) != null) {
+		if (token.kind == TokenKind.Keyword && token.value == 'if') {
+			index++;
+
+			// cond
+			if ((result = parseExpr(index, input)) != null) {
+				const cond = result[0];
+				index = result[1];
+
+				// block
+				if ((result = parseBlock(index, input)) != null) {
+					const block = result[0];
+					index = result[1];
+					return [cond, block, index];
+				}
+			}
+		}
+	}
+
+	return null;
+}
+
+/**
+ * ```text
+ * "{" <statement>... "}"
+ * ```
+*/
+function parseBlock(offset: number, input: Token[]): [AstNode[], number] | null {
+	let result;
+	let token;
+	let index = offset;
+
+	// "{"
+	if ((result = nextToken(index, input)) != null) {
+		[token, index] = result;
+		if (token.kind == TokenKind.Punctuator && token.value == '{') {
+
+			const content: AstNode[] = [];
+			while (true) {
+				// statement
+				result = parseStatement(index, input);
+				if (result == null) {
+					break;
+				}
+				content.push(result[0]);
+				index = result[1];
+			}
+
+			// "}"
+			if ((result = nextToken(index, input)) != null) {
+				[token, index] = result;
+				if (token.kind == TokenKind.Punctuator && token.value == '}') {
+					return [content, index];
+				}
+			}
+		}
 	}
 
 	return null;
